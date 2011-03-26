8a28ca546b8227ebe8e376b8150b19ecc65ee60f
Martin Provencher <vivrass@vivyMac.local>
2011-03-26 12:52:57 -0400

    Refactor and remove unuseful class

--- src/java/org/hackreduce/examples/wikipedia/TaggerMapper.java
+++ src/java/org/hackreduce/examples/wikipedia/TaggerMapper.java
@@ -16,29 +16,20 @@ import org.hackreduce.mappers.XMLRecordReader;
 import org.hackreduce.models.WikipediaRecord;
 
 
-/**
- * This MapReduce job will map Wikipedia data to actual workable data for the tagger algorithm.
- *
- */
-public class TaggerMapper extends org.hackreduce.examples.wikipedia.Tagger {
+public class TaggerMapper extends WikipediaMapper<Text, LongWritable> {
 
-	public static class WikipediaParser extends WikipediaMapper<Text, LongWritable> {
+	// Our own made up key to send all counts to a single Reducer, so we can
+	// aggregate a total value.
+	public static final Text TOTAL_COUNT = new Text("total");
 
-		// Our own made up key to send all counts to a single Reducer, so we can
-		// aggregate a total value.
-		public static final Text TOTAL_COUNT = new Text("total");
+	// Just to save on object instantiation
+	public static final LongWritable ONE_COUNT = new LongWritable(1);
 
-		// Just to save on object instantiation
-		public static final LongWritable ONE_COUNT = new LongWritable(1);
+	@Override
+	protected void map(WikipediaRecord record, Context context) throws IOException,
+			InterruptedException {
 
-		@Override
-		protected void map(WikipediaRecord record, Context context) throws IOException,
-				InterruptedException {
-
-			String text = record.getText();
-			ArrayList<String> categories = record.getCategories();
-			
-			WikipediaData wikiData = new WikipediaData(text, categories);
-		}
+		String text = record.getText();
+		ArrayList<String> categories = record.getCategories();
 	}
 }
