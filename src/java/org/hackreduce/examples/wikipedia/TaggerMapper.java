ba62b328ced955e359fe413294bbab10f2f9dfb7
Martin Provencher <vivrass@vivyMac.local>
2011-03-26 18:03:23 -0400

    LongWritable to DoubleWritable

--- src/java/org/hackreduce/examples/wikipedia/TaggerMapper.java
+++ src/java/org/hackreduce/examples/wikipedia/TaggerMapper.java
@@ -2,12 +2,15 @@ package org.hackreduce.examples.wikipedia;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.cxf.binding.http.strategy.EnglishInflector;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.hackreduce.examples.wikipedia.Tagger.TaggerCount;
 import org.hackreduce.mappers.WikipediaMapper;
@@ -22,14 +25,14 @@ import edu.northwestern.at.utils.corpuslinguistics.tokenizer.WordTokenizer;
  *
  */
 
-public class TaggerMapper extends WikipediaMapper<Text, LongWritable> {
+public class TaggerMapper extends WikipediaMapper<Text, DoubleWritable> {
 
 	// Our own made up key to send all counts to a single Reducer, so we can
 	// aggregate a total value.
 	public static final Text TOTAL_COUNT = new Text("total");
 
 	// Just to save on object instantiation
-	public static final LongWritable ONE_COUNT = new LongWritable(1);
+	public static final DoubleWritable ONE_COUNT = new DoubleWritable(1);
 
 	public static final Pattern TO_KEEP = Pattern.compile("^[a-z]+$");
 	@Override
@@ -38,16 +41,37 @@ public class TaggerMapper extends WikipediaMapper<Text, LongWritable> {
 			String text = record.getRawText();
 			ArrayList<String> categories = record.getCategories();
 
+			if ( categories.size() == 0)
+			{
+				context.getCounter(TaggerCount.ARTICLES_IGNORED).increment(1);
+				return;
+			}
+
 			EnglishInflector eng = new EnglishInflector();
 			WordTokenizer tokenizer = new DefaultWordTokenizer();
 			List<String> words = tokenizer.extractWords(text);
+			Hashtable<String, Long> uniqueValues = new Hashtable<String, Long>();
 
+			long currentCount = 0;
 			for(String word : words) {
 				word = eng.singularlize(word).toLowerCase();
 				if (TO_KEEP.matcher(word).matches()) {
-					for (String category : categories) {
-						context.write(new Text(category + ":" + word), new LongWritable(1));
-					}
+					if( uniqueValues.containsKey(word) )
+						currentCount = (Long) uniqueValues.get(word) + 1;
+					else
+						currentCount = 1;
+					uniqueValues.put(word, currentCount);
+				}
+			}
+
+			Enumeration<String> keys = uniqueValues.keys();
+			String key;
+			int count = words.size();
+			while(keys.hasMoreElements()) {
+				key = keys.nextElement();
+				double value = ((double)uniqueValues.get(key))/count;
+				for (String category : categories) {
+					context.write(new Text(category + ":" + key), new DoubleWritable(value));
 				}
 			}
 
